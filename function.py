9e5fa39a822ff55bbcb4833faedeb370fc41f8f9
Dimonchik <sudjadred@live.com>
2023-01-10 18:52:35 +0200

    add_function_update1

--- function.py
+++ function.py
@@ -1,4 +1,7 @@
 import csv
+from telegram import Update
+from telegram.ext import CallbackContext
+import os.path
 
 db = []
 id = 0
@@ -7,16 +10,19 @@ def init_db(file_name='DB.csv'):
     global db
     db_file_name = file_name
     db.clear()
+    if os.path.exists(db_file_name):
+        with open(db_file_name, 'r', newline='') as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                if(row[0] != 'ID'):
+                    db.append(row)
+                    # if(int(row[0]) > id):
+                    #     id = int(row[0])
+            else:
+                open(db_file_name, 'w', newline='').close()
+    return None
 
-    with open(db_file_name, 'r', newline='') as csv_file:
-        reader = csv.reader(csv_file)
-        for row in reader:
-            if(row[0] != 'ID'):
-                db.append(row)
-                if(int(row[0]) > id):
-                    id = int(row[0])
-
-def show_db(file_name='DB.csv'):
+def show_db(update: Update, context: CallbackContext, file_name='DB.csv'):
     with open(file_name) as f:
         reader = csv.reader(f,delimiter='|')
         headers = next(reader)
@@ -24,34 +30,34 @@ def show_db(file_name='DB.csv'):
         for row in reader:
             print(row)
 
-def write_db():
-    global id
-    global count
-    input_data = []
-    
-    id =+ 1
-    input_data.append(id)
 
+def write_db(update: Update, context: CallbackContext):
+    global id
+    global db
+    
+    id += 1
+    db.append(id)
+        
     first_name = input('Введите имя: ')
     if(first_name  == ''):
         print('Поле не может быть пустым!')
         return
-    input_data.append(first_name)
+    db.append(first_name)
     last_name = input('Введите Фамилию: ')
     if(last_name == ''):
         print('Поле не может быть пустым!')
         return
-    input_data.append(last_name)   
+    db.append(last_name)   
     phone_num = give_int('Введите телефон: ')
     if(phone_num  == ''):
         print('Поле не может быть пустым!')
         return        
-    input_data.append(phone_num)
+    db.append(phone_num)
     comment = input('Введите комментарий: ')
-    input_data.append(comment)
+    db.append(comment)
 
     file = open ('DB.csv', 'a')
-    file.write(f'{input_data[0]}|{input_data[1]}|{input_data[2]}|{input_data[3]}|{input_data[4]}\n')
+    file.write(f'{db[0]}|{db[1]}|{db[2]}|{db[3]}|{db[4]}\n')
     file.close()
 
 def give_int(input_number) -> int:
